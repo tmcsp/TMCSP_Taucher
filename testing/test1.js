ad45060863e0eeb5fc7a16a0600365e01717abf6
tmcsp <2120781001@fh-burgenland.at>
2022-06-14 13:22:16 +0000

    changes

---
+++ testing/test1.js
@@ -0,0 +1,7 @@
+const assert = require('assert'); 
+const calculator = require('../src/calculator'); 
+describe('Simple Math Test', () => { 
+    it('should return 2', () => { 
+        assert.equal(1 + 1, 2);
+     });
+});
